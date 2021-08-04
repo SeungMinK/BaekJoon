6833b850849616217dba6c32e7fa1f041320ca0e
SeungMinKim <ksm8797@naver.com>
2021-08-04 21:38:26 +0900

    Update Code

--- prob_1000-9999/Step1/10869.js
+++ prob_1000-9999/Step1/10869.js
@@ -1,8 +1,20 @@
-const fs = require('fs')
-const inputNum = fs.readFileSync().toString().split(' ').map(value => +value)
+//입력값 받기, 같은 폴더내에 input.txt 생성
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split(' ');
 
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(Math.floor(a/b))
-console.log(a%b)
+
+solution(input[0],input[1]);
+
+function solution(A, B) {
+
+A=parseInt(A);
+B=parseInt(B);
+
+ console.log(A+B);
+ console.log(A-B);
+ console.log(A*B);
+ console.log(parseInt(A/B));
+ console.log(A%B);
+
+}
