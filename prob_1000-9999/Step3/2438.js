39220a21f088c2f5ceab3c0c5210f2fb54da6e4d
SeungMinKim <ksm8797@naver.com>
2021-08-07 23:35:41 +0900

    [add file] 3단계 9번

---
+++ prob_1000-9999/Step3/2438.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+
+solution(input);
+
+
+function solution(N) {
+    N = parseInt(N);
+    let resultString ='';
+    for(var i= 1; i<=N; i++){
+
+        for(var j=0;j<i;j++){
+            resultString += '*';
+        }
+        console.log(resultString);
+        resultString ='';
+    }
+
+
+}
