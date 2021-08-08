a35d64c6111776ce425c4b8aa86c851b044c65ff
SeungMinKim <ksm8797@naver.com>
2021-08-08 23:34:45 +0900

    [add file] 3단계 11번

---
+++ prob_1000-9999/Step3/10871.js
@@ -0,0 +1,12 @@
+//입력값 받기, 같은 폴더내에 input.txt 생성
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split(' ');
+
+
+solution(input[0]);
+
+function solution(N) {
+
+    
+}
