de1c6a4c22c6e5393cd602a2a7736a937635b832
SeungMinKim <ksm8797@naver.com>
2021-08-12 19:01:57 +0900

    [update] 3단계 11번 업데이트

--- prob_1000-9999/Step3/10871.js
+++ prob_1000-9999/Step3/10871.js
@@ -1,12 +1,32 @@
 //입력값 받기, 같은 폴더내에 input.txt 생성
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().split(' ');
+let input = fs.readFileSync(filePath).toString().split('\n');
 
 
-solution(input[0]);
+solution(input);
 
-function solution(N) {
+function solution(input) {
 
     
+    let temp = input[0].split(' ');
+    let tempArr = input[1].split(' ');
+    
+    let N = parseInt(temp[0]);
+    let X = parseInt(temp[1]);
+   
+    let result = '';
+    
+    Arr = tempArr
+            .map(str => str.split(' '))
+            .map(str => parseInt(str));
+    
+    for(let i = 0; i < N; i++){
+    
+        Arr[i] < X ? result+=Arr[i]+' ':  '';
+
+    }
+    console.log(result);
+
+
 }
