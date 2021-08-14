9cf3dfdc4f9b8557c1e361866b0fffb19bfde96c
SeungMinKim <ksm8797@naver.com>
2021-08-14 14:40:38 +0900

    [add file] 5단계 2번

---
+++ prob_1000-9999/Step5/2562.js
@@ -0,0 +1,18 @@
+//입력값 받기, 같은 폴더내에 input.txt 생성
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+
+solution(input);
+
+function solution(input) {
+    let max=+input[0];
+    for(var i= 0; i<input.length;i++){
+        max = +input[i]>max ? +input[i] : max;
+    }
+    console.log(max);
+    console.log(input.indexOf(max+'')+1);
+
+}
+
