6d0851c9a8b483fd1432bbf61e0ef824e28d8328
SeungMinKim <ksm8797@naver.com>
2021-08-14 19:59:05 +0900

    [temp] temp commit

--- prob_1000-9999/Step5/2562.js
+++ prob_1000-9999/Step5/2562.js
@@ -3,17 +3,13 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
 
-
 solution(input);
 
 function solution(input) {
-    let max=+input[0];
-    for(var i= 0; i<input.length;i++){
-        max = +input[i]>max ? +input[i] : max;
-    }
-    console.log(max);
-    console.log(input.indexOf(max+'')+1);
-    
-
+  let max = +input[0];
+  for (var i = 0; i < input.length; i++) {
+    max = +input[i] > max ? +input[i] : max;
+  }
+  console.log(max);
+  console.log(input.indexOf(max + '') + 1);
 }
-
