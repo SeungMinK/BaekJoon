5dc7b3c1e5f8d9a5ef0f94713cbba23650ddacc9
SeungMinKim <ksm8797@naver.com>
2021-08-20 23:09:50 +0900

    [update] 5단계 7번

--- prob_1000-9999/Step5/4344.js
+++ prob_1000-9999/Step5/4344.js
@@ -10,17 +10,24 @@ function solution(input) {
   let array = input.splice(1); // 인덱스가 1인값 = Size 를 삭제, 즉 input[0] 빼고 모든 값이 들어감
 
   for (let i = 0; i < C; i++) {
+    //지역변수로 선언
     let temp = array[i].split(' ');
     let sum = 0;
     let count = 0;
     let avg = 0;
-    for (let j = 0; j < temp[i][0]; j++) {
-      sum += +temp[i][j];
+    // 평균 구하기
+    for (let j = 1; j <= +temp[0]; j++) {
+      sum += +temp[j];
     }
-    avg = sum / temp[i][0];
-    for (let j = 0; j < temp[i][0]; j++) {
-      count += avg >= temp[i][j] ? 1 : 0;
+    avg = sum / +temp[0];
+
+    //평균 넘는값 구하기
+    for (let j = 1; j <= +temp[0]; j++) {
+      avg < temp[j] ? count++ : '';
     }
-    console.log(count);
+
+    //.toFixed(N) N번째 까지만 표시 나머지는 값은 반올림 하여 처리
+    let result = (count / temp[0]) * 100;
+    console.log(`${result.toFixed(3)}%`);
   }
 }
