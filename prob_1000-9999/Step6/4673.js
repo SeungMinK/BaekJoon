cd5ef1b81377e4bcbe4b0716c0a426d58bbac867
SeungMinKim <ksm8797@naver.com>
2021-08-24 22:53:37 +0900

    [update code] 쓸데 없는 반복문 제거, 중복으로 값 체크하는 현상 방지

--- prob_1000-9999/Step6/4673.js
+++ prob_1000-9999/Step6/4673.js
@@ -3,42 +3,36 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
 */
+const iter = 10000; //문제에서 10000으로 범위 제한
 solution();
 
 function solution() {
-  /*
-셀프 넘버는 1949년 인도 수학자 D.R. Kaprekar가 이름 붙였다. 양의 정수 n에 대해서 d(n)을 n과 n의 각 자리수를 더하는 함수라고 정의하자. 예를 들어, d(75) = 75+7+5 = 87이다.
-양의 정수 n이 주어졌을 때, 이 수를 시작해서 n, d(n), d(d(n)), d(d(d(n))), ...과 같은 무한 수열을 만들 수 있다. 
-예를 들어, 33으로 시작한다면 다음 수는 33 + 3 + 3 = 39이고, 그 다음 수는 39 + 3 + 9 = 51, 다음 수는 51 + 5 + 1 = 57이다. 이런식으로 다음과 같은 수열을 만들 수 있다.
-33, 39, 51, 57, 69, 84, 96, 111, 114, 120, 123, 129, 141, ...
-n을 d(n)의 생성자라고 한다. 위의 수열에서 33은 39의 생성자이고, 39는 51의 생성자, 51은 57의 생성자이다. 생성자가 한 개보다 많은 경우도 있다. 예를 들어, 101은 생성자가 2개(91과 100) 있다. 
-생성자가 없는 숫자를 셀프 넘버라고 한다. 100보다 작은 셀프 넘버는 총 13개가 있다. 1, 3, 5, 7, 9, 20, 31, 42, 53, 64, 75, 86, 97
-10000보다 작거나 같은 셀프 넘버를 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
-*/
-  let numArray = new Array(10000);
-  for (let i = 0; i < 10000; i++) {
-    numArray[i] = true;
+  //모든값을 true로 초기화
+  let numArray = Array.from({ length: 10000 }, () => true);
+
+  //셀프 넘버가 있는지 체크하기
+  for (let i = 1; i < iter; i++) {
+    if (numArray[i] != false) selfNumCheck(i + 1); // 이미 체크한 값은 중복으로 처리 안하기
+
+    numArray[selfNumCheck(i) - 1] = false; //10000 이하의 생성자는 모두 false로 처리
   }
 
-  for (let i = 1; i < 10000; i++) {
-    selfNumCheck(i + 1);
-    numArray[selfNumCheck(i) - 1] = false;
-  }
-
-  for (let i = 0; i < 10000; i++) {
+  //결과 출력
+  for (let i = 0; i < iter; i++) {
     if (numArray[i]) console.log(i + 1);
   }
 }
+
 function selfNumCheck(num) {
   let temp = num;
-  let size = (temp + '').length;
+  let size = (temp + '').length; //넘어온 숫자의 자릿수 ex) 7 = 한자릿수, 18 = 두자릿수, 182 = 세자릿수
   let result = num;
 
   for (let i = 0; i < size; i++) {
     result += +(temp + '')[i];
   }
-
-  if (result < 10000) selfNumCheck(result);
+  //재귀 함수로 구현, iter값 까지 모든 생성자를 찾음
+  if (result < iter) selfNumCheck(result);
 
   return result;
 }
