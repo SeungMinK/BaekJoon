2cc3bec12d362059eda1a313085f7dd103bb1c00
SeungMinK <ksm8797@naver.com>
2021-09-01 14:08:46 +0900

    [update code] 시간 단축

--- prob_1000-9999/Step7/1157.js
+++ prob_1000-9999/Step7/1157.js
@@ -7,19 +7,20 @@ solution(input);
 
 function solution(input) {
   let arrAlpha = Array.from({ length: 26 }, () => 0);
-  let size = (input + '').length;
+  let textSize = (input + '').length;
+  let size = arrAlpha.length;
   let text = (input + '').split('');
   let check = false;
 
   //모든 값 대문자로 바꾸고, alpha case에 해당하는 배열 값 올려주기
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < textSize; i++) {
     arrAlpha[text[i].toUpperCase().charCodeAt(0) - 65]++;
   }
   let maxCount = 0;
   let result = 0;
 
   //값이 제일 많은 아스키코드와, max값이 몇개인지 체크
-  for (let j = 0; j < arrAlpha.length; j++) {
+  for (let j = 0; j < size; j++) {
     if (maxCount < arrAlpha[j]) {
       result = j;
       maxCount = arrAlpha[j];
@@ -27,7 +28,7 @@ function solution(input) {
   }
 
   //max값이 2개 이상이면 '?' 아스키코드값인 63을 만들기위해 -2 를 대입
-  for (let k = 0; k < arrAlpha.length; k++) {
+  for (let k = 0; k < size; k++) {
     if (arrAlpha[k] == maxCount) {
       if (check == false) check = true;
       else result = -2;
